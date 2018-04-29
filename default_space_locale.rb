e9dcb7815246ab14bc949d642b32f4d41a2aac96
Mike Jerome <m@j4e.name>
2018-04-29 21:09:42 +0100

    Remove file

--- default_space_locale.rb
+++
@@ -1,6 +0,0 @@
-require "contentful"
-client = Contentful::Client.new(
-  access_token: "ba4951e8a62ac32ea80edcd0655aadb2e8bcd5656ef15a82aae6cdd290c88925",
-  space: "e3yk46djt0o5",
-)
-puts client.space.locales.find { |e| e.default == true }.code
