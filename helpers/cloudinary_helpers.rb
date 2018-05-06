59429a3a6ba3a01ee6e5529895bd1a1122b48435
Mike Jerome <m@j4e.name>
2018-05-06 23:29:21 +0100

    Add Contact Us Page with JSON LD data

--- helpers/cloudinary_helpers.rb
+++ helpers/cloudinary_helpers.rb
@@ -1,7 +1,11 @@
 module CloudinaryHelpers
   def cloudinary_image_tag(source, options = {})
+    image_tag(cloudinary_image_url(source), options)
+  end
+
+  def cloudinary_image_url(source)
     cloudinary_cloud_name = ENV["CLOUDINARY_CLOUD_NAME"]
     fetch = "https://res.cloudinary.com/#{cloudinary_cloud_name}/image/fetch/f_auto,q_auto/https:"
-    image_tag(fetch + source, options)
+    fetch + source
   end
 end
