2838adda8df63285a8cfdfe5e080f241185d63f9
Mike Jerome <m@j4e.name>
2018-05-07 08:57:06 +0100

    Web page icons

--- helpers/cloudinary_helpers.rb
+++ helpers/cloudinary_helpers.rb
@@ -3,9 +3,10 @@ module CloudinaryHelpers
     image_tag(cloudinary_image_url(source), options)
   end
 
-  def cloudinary_image_url(source)
+  def cloudinary_image_url(source, cloudinary_options=[])
     cloudinary_cloud_name = ENV["CLOUDINARY_CLOUD_NAME"]
-    fetch = "https://res.cloudinary.com/#{cloudinary_cloud_name}/image/fetch/f_auto,q_auto/https:"
+    o = (['f_auto', 'q_auto'] + cloudinary_options).join(',')
+    fetch = "https://res.cloudinary.com/#{cloudinary_cloud_name}/image/fetch/#{o}/https:"
     fetch + source
   end
 end
